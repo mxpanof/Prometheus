2181d5ee3cf81e7b76f60bb37d338c3975720ca0
Max Panoff <mpanoff@stevens.edu>
2017-12-07 09:52:03 -0500

    First upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/max/catkin_ws/src
+# Build directory: /home/max/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3/turtlebot3")
+subdirs("turtlebot3/turtlebot3_bringup")
+subdirs("turtlebot3_msgs")
+subdirs("turtlebot3/turtlebot3_slam")
+subdirs("turtlebot3/turtlebot3_teleop")
+subdirs("turtlebot3/turtlebot3_navigation")
+subdirs("turtlebot3/turtlebot3_description")
